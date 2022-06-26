6153ee6c4c4b304b7ed0216260e3576565ab6a61
644708020013 <107332352+644708020013@users.noreply.github.com>
2022-06-26 09:54:04 +0700

    my_math

---
+++ my_math_use.py
@@ -0,0 +1,11 @@
+import my_math
+from my_math import sqrt,circle_area
+
+print(f'sqrt of 5 = {my_math.sqrt(5)}')
+print(f'circle area = {my_math.circle_area(2):,.2f}')
+
+#วิธีที่2
+import my_math
+print("****วิธีที่2****")
+print(f'sqrt of 5 = {my_math.sqrt(5)}')
+print(f'circle area = {my_math.circle_area(2):,.2f}')
